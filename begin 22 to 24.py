3052923bdb8d850ce05a5024da3bab65181a157c
Sohibnazar <115521214+Sohibnazar@users.noreply.github.com>
2022-11-15 16:07:47 +0500

    Add files via upload

---
+++ begin 22 to 24.py
@@ -0,0 +1,27 @@
+#begin-22
+def pereme_1(A,B):
+    temp=A
+    A=B
+    B=temp
+    
+    print(A,B)
+pereme_1(4,5)
+#begin-23
+def perem_2(A,B,C):
+    temp=A
+    A=B
+    B=C
+    C=temp
+    print(A,B,C)
+perem_2(6,8,9)
+#begin-24
+def perem_3(A,B,C):
+    temp=A
+    A=C
+    C=B
+    B=temp
+    print(A,B,C)
+perem_3(6,8,9)
+    
+    
+

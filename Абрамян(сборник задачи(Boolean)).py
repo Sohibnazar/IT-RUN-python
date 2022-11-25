e477d7a8b402ca162c9aa710f0806ff826a6a25f
Sohibnazar <115521214+Sohibnazar@users.noreply.github.com>
2022-11-25 21:15:00 +0500

    Add files via upload

---
+++ Абрамян(сборник задачи(Boolean)).py
@@ -0,0 +1,113 @@
+#Boolean - 1
+print("Boolean-1")
+def polozh_1(A):
+    print(A>0)
+polozh_1(885)
+#Boolean - 2
+print("Boolean-2")
+def chet_1(A):
+    print( A%2!=0)
+chet_1(67)
+#Boolean - 3
+print("Boolean-3")
+def nechet_1(A):
+    print( A%2==0)
+nechet_1(988)
+#Boolean - 4
+print("Boolean-4")
+def neravenstv_1(A,B):
+    print(A>2 and B<=3 )
+neravenstv_1(2,3)
+#Boolean - 5
+print("Boolean-5")
+def neravenstv_2(A,B):
+    print( A>=0 or B<-2)
+neravenstv_2(5,3)
+#Boolean - 6
+print("Boolean-6")
+def neravenstv_1(A,B,C):
+    print(A<B and B<C or A>B and B>C)
+neravenstv_1(2,3,7)
+
+#Boolean - 7
+print("Boolean-7")
+def neravenstv_2(A,B,C):
+    print(  B>A and B<C or B<A and B>C)
+neravenstv_2(90,300,700)
+
+#Boolean - 8
+print("Boolean-8")
+def nechet_1(A,B):
+    print( A%2==1 and B%2==1)
+nechet_1(3,7)
+
+
+#Boolean - 9
+print("Boolean-9")
+def nechet_2(A,B):
+    
+        print(  A%2>0 or B%2>0)
+nechet_2(10,7)
+
+#Boolean - 10
+print("Boolean-10")
+def nechet_3(A,B):
+    print(  A%2!=0 and B%2==0)
+nechet_3(90,37)
+#Boolean - 10
+print("Boolean-10")
+def nechet_3(A,B):
+    print(  A%2!=0 and B%2==0)
+nechet_3(90,37)
+#Boolean - 11
+print("Boolean-11")
+def nechet_4(A,B):
+    print(A%2>0 ==B%2>0)
+nechet_4(95,36)
+#Boolean - 12
+print("Boolean-12")
+def chislo_1(A,B,C):
+    print(A>0 and B>0 and C>0)
+chislo_1(95,36,780)
+#Boolean - 13
+print("Boolean-13")
+def chislo_2(A,B,C):
+    print(A>0 or B>0 or C>0)
+chislo_2(5,6,0)
+#Boolean - 14
+print("Boolean-14")
+def chislo_3(A,B,C):
+    print(A>0 and B>=0 and C>=0 and A>=0 and B>0 and C>=0 and A>=0 and B>=0 and C>0)  
+chislo_3(95,36,780)
+#Boolean - 15
+print("Boolean-15")
+def chislo_3(A,B,C):
+    print(A>0 and B>0 and C>=0 and A>=0 and B>0 and C>0 and A>0 and B>0 and C>=0)  
+chislo_3(-95,36,780)
+
+#boolean -16
+print("Boolean-16")
+def chet_1(A):
+    print((A>9) and (A<100) and A%2==0)
+chet_1(66)
+
+#boolean -17
+print("Boolean-17")
+def chet_1(A):
+    print((A>99)and (A<1000) and A%2!=0)
+chet_1(666)
+#boolean -18
+print("Boolean-18")
+def chi_1(A,B,C):
+    print((A==B)or(B==C)or(C==A))
+chi_1(6,7,6)
+#boolean -19
+print("Boolean-19")
+def chi_1(A,B,C):
+    print((A==(-1)*B)or(B==(-1)*C)or(C==(-1)*A))
+chi_1(5,7,-6)
+#boolean -20
+print("Boolean-20")
+def chi_1(A):
+    print((A/100)!=(A%100)/10)and(A/100)!=(A%10)and (A%100/10)!=(A%10)
+chi_1(554)
